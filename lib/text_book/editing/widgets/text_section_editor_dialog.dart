ec8802599ca1fb2b92e41666a7d97239545a3e97
Moshe-shmuel <MS123123@outlook.co.il>
2026-01-14 11:55:59 +0200

    הההההההההההההההההה

--- lib/text_book/editing/widgets/text_section_editor_dialog.dart
+++ lib/text_book/editing/widgets/text_section_editor_dialog.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:isolate';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart'; // הוספת import עבור SelectedContent
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
